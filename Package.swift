fabfacb9909fdc86b529077b91240c16bec837a1
EbrahimTahernejad <EbrahimTahernejad@users.noreply.github.com>
2023-09-29 03:43:19 +0000

    TLSServer Release 0.1.1

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "TLSServer",
-      url: "https://github.com/EbrahimTahernejad/TLSServer/releases/download/0.1.0/TLSServer.xcframework.zip",
-      checksum: "2687c840ddb8cfeb393332d1e8a4ae7c2d52200f12ff7c85688a04ecae66d8b9"
+      url: "https://github.com/EbrahimTahernejad/TLSServer/releases/download/0.1.1/TLSServer.xcframework.zip",
+      checksum: "40c66b30894896be471afae714f6797ba3b14f0fbc452d68ab235dc269edc8a9"
     )
   ]
 )
