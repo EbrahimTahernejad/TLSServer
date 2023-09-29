73fff4324ff1134c73faf7212d7a12896bfbd411
Ebrahim Tahernejad <ebrahimtahernejad@gmail.com>
2023-09-29 06:55:28 +0330

    Removed extra param

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 mkdir -p build
-gomobile bind -iosversion=12.0 -macos-version=12.0 -target=ios,iossimulator,macos -o build/TLSServer.xcframework .
+gomobile bind -iosversion=12.0 -target=ios,iossimulator,macos -o build/TLSServer.xcframework .
